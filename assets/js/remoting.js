9442d38afaaa0fbd7174720f2d25aaba327a6cb5
Markus Kobler <markus@distinctive.co>
2012-09-21 18:00:00 +0100

    .

--- assets/js/remoting.js
+++ assets/js/remoting.js
@@ -1,49 +1,67 @@
+(function(window, Pusher) {
+
+  var pusher = new Pusher('10408a3c620b760edd45')
+    , channel = null;
+
+  // User details need to be set by mobile app
+  var user = {id:'123',
+              name:"Jo Blogs",
+              picture:"https://secure.gravatar.com/avatar/83495e6101c2561c3f7332db3bbe82f9?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png"};
+
+
+//  FB.init({ appId: "422104674519024", nativeInterface: CDV.FB, useCachedDialogs: false });
+
+//  window.fbAsyncInit = function() {
+//    FB.init({
+//      appId      : '422104674519024', // App ID
+//      channelUrl : '//localhost:3000/', // Channel File
+//      status     : true // check login status
+//    });
+//
+//    // Additional initialization code here
+//  };
+
+  function printMove(move) {
+    var messages = document.getElementById("message");
+    messages.innerHTML += "<strong>"+move.user.name+"</strong>, moved: "+move.x+":"+move.y+"<br/>";
+  }
+
+  function printJoined(user) {
+    var messages = document.getElementById("message");
+    messages.innerHTML += "<strong>"+user.name+"</strong>, joined<br/>";
+  }
+
+  function initRemoting(user) {
+    $.ajax("http://potatobook.herokuapp.com/api/start_game")
+      .done(function(data) {
+        Pusher.channel_auth_endpoint = 'http://potatobook.herokuapp.com/api/pusher_auth';
+
+        channel = pusher.subscribe(data.channel);
+
+        channel.bind('pusher:subscription_succeeded', function() {
+          channel.trigger('client-joined', user);
+          printJoined(user);
+        });
+
+        channel.bind('client-joined', printJoined);
+
+        channel.bind('client-moved', printMove);
+      });
+  }
+
+  initRemoting(user);
+
+  function sendMove() {
+    var move = {x: Math.floor(Math.random()*5),
+                y:Math.floor(Math.random()*5),
+                user: user};
+    printMove(move);
+    channel.trigger('client-moved', move);
+  }
+
+})(window, Pusher);
+
 // Enable pusher logging - don't include this in production
 Pusher.log = function(message) {
   if (window.console && window.console.log) window.console.log(message);
 };
-// Flash fallback logging - don't include this in production
-WEB_SOCKET_DEBUG = true;
-
-Pusher.channel_auth_endpoint = '/api/pusher_auth';
-var pusher = new Pusher('10408a3c620b760edd45');
-
-function printMove(move) {
-  var messages = document.getElementById("message");
-  messages.innerHTML += "<strong>"+move.user.name+"</strong>, moved: "+move.x+":"+move.y+"<br/>";
-}
-
-function printJoined(user) {
-  var messages = document.getElementById("message");
-  messages.innerHTML += "<strong>"+user.name+"</strong>, joined<br/>";
-}
-
-var channel = null;
-
-function initRemoting(user) {
-
-  $.ajax("/api/start_game")
-    .done(function(data) {
-      channel = pusher.subscribe(data.channel);
-      channel.bind('pusher:subscription_succeeded', function(data) {
-        channel.trigger('client-joined', user);
-        printJoined(user);
-      });
-      channel.bind('client-joined', printJoined);
-      channel.bind('client-moved', printMove);
-    });
-}
-
-// User details need to be set by mobile app
-var user = {name:"Jo Blogs",
-  picture:"https://secure.gravatar.com/avatar/83495e6101c2561c3f7332db3bbe82f9?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png"};
-
-initRemoting(user);
-
-function sendMove() {
-  var move = {x: Math.floor(Math.random()*5),
-              y:Math.floor(Math.random()*5),
-              user: user};
-  printMove(move);
-  channel.trigger('client-moved', move);
-}
