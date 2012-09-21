e32cf0633544759650f45732e81783f623bb795a
Markus Kobler <markus@distinctive.co>
2012-09-21 18:30:04 +0100

    Fixing remoting

--- assets/js/remoting.js
+++ assets/js/remoting.js
@@ -8,19 +8,6 @@
               name:"Jo Blogs",
               picture:"https://secure.gravatar.com/avatar/83495e6101c2561c3f7332db3bbe82f9?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-org-420.png"};
 
-
-//  FB.init({ appId: "422104674519024", nativeInterface: CDV.FB, useCachedDialogs: false });
-
-//  window.fbAsyncInit = function() {
-//    FB.init({
-//      appId      : '422104674519024', // App ID
-//      channelUrl : '//localhost:3000/', // Channel File
-//      status     : true // check login status
-//    });
-//
-//    // Additional initialization code here
-//  };
-
   function printMove(move) {
     var messages = document.getElementById("message");
     messages.innerHTML += "<strong>"+move.user.name+"</strong>, moved: "+move.x+":"+move.y+"<br/>";
@@ -44,7 +31,6 @@
         });
 
         channel.bind('client-joined', printJoined);
-
         channel.bind('client-moved', printMove);
       });
   }
